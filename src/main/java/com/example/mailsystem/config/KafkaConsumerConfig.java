9f07e05cd471ff034d40674b7dd0d6c299230c02
NaGeTa <dagestan200221@mail.ru>
2024-01-14 14:39:04 +0300

    kafka fix

--- src/main/java/com/example/mailsystem/config/KafkaConsumerConfig.java
+++ src/main/java/com/example/mailsystem/config/KafkaConsumerConfig.java
@@ -38,7 +38,7 @@ public class KafkaConsumerConfig {
         Map<String, Object> props = new HashMap<>();
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "123");
+//        props.put(ConsumerConfig.GROUP_ID_CONFIG, "123");
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         return props;
--- src/main/java/com/example/mailsystem/config/KafkaListenerConfig.java
+++ src/main/java/com/example/mailsystem/config/KafkaListenerConfig.java
@@ -1,5 +1,6 @@
 package com.example.mailsystem.config;
 
+import com.example.mailsystem.model.ResetForMail;
 import com.example.mailsystem.model.SolutionForMail;
 import com.example.mailsystem.service.MailService;
 import com.fasterxml.jackson.core.JsonProcessingException;
@@ -8,19 +9,30 @@ import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.KafkaListener;
 
-//TODO
-//@Configuration
+
+@Configuration
 @AllArgsConstructor
 public class KafkaListenerConfig {
 
     private final ObjectMapper objectMapper;
     private final MailService mailService;
 
-    @KafkaListener(topics = "mailTopic", containerFactory = "myListenerContainerFactory")
-    public void listen(String data) {
+    @KafkaListener(topics = "resultTopic", /*containerFactory = "myListenerContainerFactory",*/ groupId = "resultGroup")
+    public void resultListen(String data) {
         System.out.println(data + "\n");
         try {
             mailService.sendSolution(objectMapper.readValue(data, SolutionForMail.class));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @KafkaListener(topics = "resetTopic",/* containerFactory = "myListenerContainerFactory",*/ groupId = "resetGroup")
+    public void resetListen(String data) {
+        System.out.println(data + "\n");
+        try {
+            mailService.sendReset(objectMapper.readValue(data, ResetForMail.class));
+            System.out.println("OKKKKKKK");
         } catch (JsonProcessingException e) {
             throw new RuntimeException(e);
         }
